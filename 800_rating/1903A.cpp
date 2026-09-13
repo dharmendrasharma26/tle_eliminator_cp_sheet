a1b35857e86e0af470a878116d22045fb5f3f9cf
Dharmendra Sharma <dharmendrasharma.cs@gmail.com>
2026-09-13 12:08:08 +0530

    Create 1903A.cpp

---
+++ 800_rating/1903A.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, k;
+        cin>>n>>k;
+        
+        vector<int> arr(n);
+        
+        for(int i=0; i<n; i++){
+            cin>>arr[i];
+        }
+        
+        if(k >= 2){
+            cout<<"YES\n";
+        }
+        else{
+            bool sorted = true;
+            for(int i=0; i<n-1; i++){
+                if(arr[i] > arr[i+1]){
+                    sorted = false;
+                    break;
+                }
+            }
+            if(sorted){
+                cout<<"YES\n";
+            }
+            else{
+                cout<<"NO\n";
+            }
+        }
+    }
+    return 0;
+}
