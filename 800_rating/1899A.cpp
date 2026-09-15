61d2147025b8528a8b97914a4b5f9dde9e309118
Dharmendra Sharma <dharmendrasharma.cs@gmail.com>
2026-09-15 18:40:37 +0530

    Create 1899A.cpp

---
+++ 800_rating/1899A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int num;
+        cin>>num;
+        if((num+1)%3 == 0 || (num-1)%3 == 0){
+            cout<<"First";
+        }
+        else{
+            cout<<"Second";
+        }
+        cout<<endl;
+    }
+}
